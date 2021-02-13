18fda6fa35bf4cfbfdb0555a840de967717f1862
Roy Are Øvrebotten <69842808+raovrebo@users.noreply.github.com>
2021-02-13 15:45:15 +0100

    Finished question 1

---
+++ js/question-1.js
@@ -0,0 +1,11 @@
+// Question 1
+// Convert the function below to an arrow function:
+
+// function division(a, b) {
+//     return a % b; }
+
+const division = (a, b) => a % b;
+
+const result = division(8, 5); // for testing purposes only
+
+console.log(result); // for testing purposes only
